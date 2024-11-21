df5a62b4a24dd22b34026d0c13c317dd68e5f2c8
itsm30thm4n <itsm30thm4n@gmail.com>
2024-11-21 06:28:47 +0100

    Create myjs.js

---
+++ myjs.js
@@ -0,0 +1,11 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-T5H20RNT0M"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-T5H20RNT0M');
+</script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6869829256302758"
+     crossorigin="anonymous"></script>
